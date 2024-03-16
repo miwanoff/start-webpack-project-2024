dc96ee6cb1848323fe6b6b05bfb21dbefa0c6b21
miwanoff <marina.iva.noffa@gmail.com>
2024-03-16 13:19:36 +0200

    Change books

--- src/js/modules/goods.js
+++ src/js/modules/goods.js
@@ -1,9 +1,24 @@
 export default function goods() {
-  $("button").click(function () {
-    $.getJSON("books.json", function (result) {
-      $.each(result, function (i, field) {
-        $("#books").append(i + " " + field.title + "<br>");
-      });
-    });
-  });
+  function insertBooks(books) {
+    let str = `<div class="wrap">`;
+    for (let i = 0; i < books.length; i++) {
+      str += `<div class="bookWrap">`;
+      str += `<div class="image"><img src="${books[i].imageCover}" /></div>`;
+      str += `<h2>${books[i].title}</h2>`;
+      str += `<p>${books[i].author}</p>`;
+      str += `</div>`;
+    }
+    str += `</div>`;
+    document.getElementById("books").innerHTML = str;
+  }
+  async function loadBooks() {
+    let url = "books.json";
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    insertBooks(data);
+  }
+  //loadBooks();
+  document.getElementById("load").addEventListener("click", loadBooks);
 }
+
