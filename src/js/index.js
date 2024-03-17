ff9c604df5a5f121489044e3d7e08a9db399fc51
miwanoff <marina.iva.noffa@gmail.com>
2024-03-17 12:31:43 +0200

    Add window.addEventListener to goods.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
 import basket from "./modules/basket";
 import goods from "./modules/goods";
+import jQuery from "jquery";
+import scroll from "./modules/scroll";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/style.css";
 import "../css/basket.css";
 import "../css/goods.css";
+window.$ = window.jQuery = jQuery;
 window.addEventListener("DOMContentLoaded", function () {
   basket();
   goods();
+  scroll();
 });
